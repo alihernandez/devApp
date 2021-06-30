449b6d4e67b92b96f2f44e00a8bbce238b8f534c
phantomramen <ali.hernandez9two@gmail.com>
2021-06-30 15:58:37 -0600

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import Contact from './components/Contact';
+import NavBar from './components/NavBar/NavBar';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <Header />
       <ToastContainer />
      <Contact />
     </div>
