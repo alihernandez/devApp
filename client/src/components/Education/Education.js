6253e1987abeb23adca71b27e1074901d2a60cce
phantomramen <ali.hernandez9two@gmail.com>
2021-07-20 13:28:49 -0600

    added footer and got carried away

---
+++ client/src/components/Education/Education.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Education.css';
+
+const Education = () => {
+
+    return(
+        <div className="educationDiv">
+            <table >
+                <ul className="educationData">
+                <th>Education:</th>
+                <li>University of Denver - Full Stack Coding Bootcamp</li>
+                <td><a href="">Resume</a></td>
+                </ul>
+            </table>
+        </div>
+        
+    )
+}
+
+export default Education;
