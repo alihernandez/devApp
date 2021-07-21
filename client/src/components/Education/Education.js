010b5cd95242143f3340db6cd7c5b448470e90e8
phantomramen <ali.hernandez9two@gmail.com>
2021-07-21 13:20:49 -0600

    added popover and fixed some css for mobile.

--- client/src/components/Education/Education.js
+++ client/src/components/Education/Education.js
@@ -5,20 +5,25 @@ const Education = () => {
 
     return(
         <div className="educationDiv">
-            <table >
-                <ul className="educationData">
-                <th>Education:</th>
-                <li>University of Denver - Full Stack Coding Bootcamp</li>
-                <td><a href="https://docs.google.com/document/d/1HfO7EoBMV-xKG8VwBZy7KIfKpd2i5ta-Kq9GEGOsHKE/edit?usp=sharing">Resume</a></td>
-                <br />
-                <th>Contact:</th>
-                <li>ali.hernandez9two@gmail.com</li>
-                <li>Colorado</li>
+            <table className="educationData">
+                <span>Education: </span>
+                <ul>
+                    <p>University of Denver Full Stack Coding Bootcamp</p>
+                    <p>December '20</p>
                 </ul>
             </table>
+            <table className="educationData">
+                <span>Contact: </span>
+                <ul>
+                    <p>ali.hernandez9two@gmail.com</p>
+                </ul>
+            </table>
+            
         </div>
         
     )
 }
 
 export default Education;
+
+//a href="https://docs.google.com/document/d/1HfO7EoBMV-xKG8VwBZy7KIfKpd2i5ta-Kq9GEGOsHKE/edit?usp=sharing">Resume</a>
