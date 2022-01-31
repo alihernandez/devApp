3a86ee339971c82aa548b52ad2184f8a54fed339
phantomramen <ali.hernandez9two@gmail.com>
2022-01-30 20:32:20 -0700

    popovers overlapping

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -1,35 +1,33 @@
-import React from 'react';
-import { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItem from '@material-ui/core/ImageListItem';
-import ImageListItemBar from '@material-ui/core/ImageListItemBar';
+import React from "react";
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import ImageList from "@material-ui/core/ImageList";
+import ImageListItem from "@material-ui/core/ImageListItem";
+import ImageListItemBar from "@material-ui/core/ImageListItemBar";
 // import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-import { Popover, Typography } from '@material-ui/core';
-import itemData from './itemData';
-import './Portfolio.css';
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+import { Popover, Typography } from "@material-ui/core";
+import itemData from "./itemData";
+import "./Portfolio.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    overflow: 'hidden',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    overflow: "hidden",
     // backgroundColor: hsla(0, 100%, 100%, 0.5),
     height: 650,
-    margin:'4rem',
-    
+    margin: "4rem",
   },
   imageList: {
-    
     width: 725,
     height: 550,
   },
   icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
+    color: "rgba(255, 255, 255, 0.54)",
   },
 }));
 
@@ -52,73 +50,104 @@ const useStyles = makeStyles((theme) => ({
  */
 export default function TitlebarImageList() {
   const classes = useStyles();
-  const [anchor, setAnchor] = useState(null);
-  const openPopover = (event) => {
-    setAnchor(event.currentTarget);
-    console.log(event);
-  }
+  // const [anchor, setAnchor] = useState(null);
+  // const openPopover = (event) => {
+  //   setAnchor(event.currentTarget);
+  //   console.log(event);
+  // }
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
 
   return (
-    <div className='projectContains'>
-    <div className={classes.root} id="projects">
-      <h1 className="proTit" component="div">Previous Projects</h1>
-      <ImageList rowHeight={180} className={classes.imageList} id="pro">
-        <ImageListItem key="Subheader" cols={2} style={{ height: 'auto' }}>
-          
-        </ImageListItem>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img}>
-            <img src={item.img} alt={item.title} />
-            
-            <ImageListItemBar
-              title={item.title}
-              subtitle={<Button variant="contained" color="primary" href={item.link}>
-              Link
-            </Button>}
-            //   link={<Button variant="contained" color="primary" href="#contained-buttons">
-            //   Link
-            // </Button>}
-            actionIcon={
-              <IconButton 
-              aria-label={`info about ${item.description}`} 
-              className={classes.icon}
-              onClick={openPopover}
+    <div className="projectContains">
+      <div className={classes.root} id="projects">
+        <h1 className="proTit" component="div">
+          Previous Projects
+        </h1>
+        <ImageList rowHeight={180} className={classes.imageList} id="pro">
+          <ImageListItem
+            key="Subheader"
+            cols={2}
+            style={{ height: "auto" }}
+          ></ImageListItem>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img src={item.img} alt={item.title} />
+
+              <ImageListItemBar
+                title={item.title}
+                subtitle={
+                  <Button variant="contained" color="primary" href={item.link}>
+                    Link
+                  </Button>
+                }
+                //   link={<Button variant="contained" color="primary" href="#contained-buttons">
+                //   Link
+                // </Button>}
+                actionIcon={
+                  <IconButton
+                    aria-label={`info about ${item.description}`}
+                    className={classes.icon}
+                    onMouseEnter={handlePopoverOpen}
+                    onClick={handlePopoverClose}
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+              <Popover
+                // open={Boolean(anchor)}
+                // container={ImageListItemBar.title}
+                // anchorEl={anchor}
+                // anchorOrigin={{
+                //   vertical: 'top',
+                //   horizontal: 'right'
+                // }}
+                // transformOrigin={{
+                //   vertical: "bottom",
+                //   horizontal: "left",
+
+                // }}
+                // aria-label={`info about ${item.description}`}
+                // onClose={() => setAnchor(null)}>
+
+                //   <Typography variant='h6'>{`${item.description}`} </Typography>
+                id="mouse-over-popover"
+                sx={{
+                  pointerEvents: "none",
+                }}
+                open={open}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                onClose={handlePopoverClose}
+                disableRestoreFocus
               >
-                <InfoIcon />
-              </IconButton>
-              
-            }/>
-            <Popover
-            open={Boolean(anchor)}
-            container={ImageListItemBar.title}
-            anchorEl={anchor}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right'
-            }}
-            transformOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
+                <Typography sx={{ p: 1 }}>{`${item.description}`} </Typography>
+              </Popover>
 
-            }}
-            aria-label={`info about ${item.description}`} 
-            onClose={() => setAnchor(null)}>
-            
-              <Typography variant='h6'>{`${item.description}`} </Typography>
-            </Popover>
-
-
-
-            {/* <Button variant="contained" color="primary" href="#contained-buttons">
+              {/* <Button variant="contained" color="primary" href="#contained-buttons">
         Link
       </Button> */}
-      
-          </ImageListItem>
-        ))}
-      </ImageList>
-    </div>
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </div>
     </div>
   );
 }
